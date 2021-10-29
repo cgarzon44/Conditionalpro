88d1214cb0e8f556c4d23be59e27f047ce9df082
cgarzon44 <cgarzon44@gmail.com>
2021-10-29 21:43:10 +0000

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+import { right } from "@popperjs/core";
 import "../style/index.css";
 
 /**
@@ -28,15 +29,41 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  // Null Vaule Name
+  let nullFirstname = "Lucy";
+  if (variables.name === null) {
+    variables.name = nullFirstname;
+  }
+
+  let nulllastname = "Bennot";
+  if (variables.lastname === null) {
+    variables.lastname = nulllastname;
+  }
+  // Null Vaule Role
+  let nullrole = "Web Developer";
+  if (variables.role === null) {
+    variables.role = nullrole;
+  }
+  // Null Vaule Location
+  let nullcity = "Miami";
+  if (variables.city === null) {
+    variables.city = nullcity;
+  }
+
+  let nullcountry = "USA";
+  if (variables.country === null) {
+    variables.country = nullcountry;
+  }
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <img src="${variables.avatarURL}" class="photo" />
+          <h1>${variables.name} ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class = ${variables.socialMediaPosition}>
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -49,6 +76,7 @@ function render(variables = {}) {
 /**
  * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
+
 window.onload = function() {
   window.variables = {
     // if includeCover is true the algorithm should
@@ -58,7 +86,7 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "positon-right",
     // social media usernames
     twitter: null,
     github: "alesanchezr",
@@ -70,6 +98,7 @@ window.onload = function() {
     country: null,
     city: null
   };
+
   render(window.variables); //render the card for the first time
 
   document.querySelectorAll(".picker").forEach(function(elm) {
@@ -85,6 +114,7 @@ window.onload = function() {
           : this.value == "false"
           ? false
           : this.value;
+
       render(Object.assign(window.variables, values)); // render again the card with new valus
     });
   });
